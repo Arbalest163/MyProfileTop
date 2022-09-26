de6717ee11715aa25056b4354d73a902c3d57447
Arbalest163 <Evil_xacker@mail.ru>
2022-09-26 21:24:09 +0400

    Добавьте файлы проекта.

---
+++ MyProfile/Models/WritingOnTheWall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProfile.Models
+{
+    public class WritingOnTheWall
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
